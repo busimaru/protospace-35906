f7b692258fe7f14db135250e1eaa02f1f566fe34
busimaru <ntdexam@icloud.com>
2021-07-25 17:00:36 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,51 @@
+class PrototypesController < ApplicationController
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+      if @prototype.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless (current_user.id == @prototype.user_id)
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+def destroy
+  prototype = Prototype.find(params[:id])
+  prototype.destroy
+  redirect_to root_path
+end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title,:concept,:catch_copy, :image).merge(user_id: current_user.id)
+  end
+end
